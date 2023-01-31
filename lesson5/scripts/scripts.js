9382cf545c80d5bf4ce572fb71d8e532a990e199
GMSTER22 <gmster22@gmail.com>
2023-01-30 21:57:34 -0500

    Completed lesson 5 DOM manipulation

---
+++ lesson5/scripts/scripts.js
@@ -0,0 +1,34 @@
+
+const input = document.querySelector( 'input' );
+
+const list = document.querySelector( 'ul' );
+
+const button = document.querySelector( 'button' );
+
+button.addEventListener( 'click', () => {
+
+  if ( ! input.value ) {
+    alert( 'Input is empty!' );
+    return;
+  }
+
+  const newChapter = input.value;
+  input.value = '';
+
+  const listItem = document.createElement( 'li' );
+  const listText = document.createElement( 'span' );
+  const listBtn = document.createElement( 'button' );
+
+  listText.textContent = newChapter;
+  listBtn.textContent = 'X';
+  listBtn.classList.add( 'delete' );
+
+  listItem.append( listText, listBtn );
+
+  list.appendChild( listItem );
+
+  listBtn.addEventListener( 'click', () => listItem.remove() );
+
+  input.focus();
+
+} );
